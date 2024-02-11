d13a006e64987eeb412013083b9e0d65873a99cc
rx987 <cxrseq9876@gmail.com>
2024-02-11 18:46:38 +0530

    src: billy ichiban

---
+++ random-solve/p4a.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/problemset/problem/4/A
+
+
+def main():
+    w = int(input())
+    if w % 2 == 0 and w > 2:
+        print("YES")
+    else:
+        print("NO")
+
+
+if __name__ == "__main__":
+    main()
